3c3e04f01fcdf8c3a54cd5bc3b4d3c214f184f54
Aneil Mallavarapu <aneil@moby.local>
2010-09-22 07:55:08 -0700

    added --daemonize, --stop options

--- djcelery/management/commands/celeryd.py
+++ djcelery/management/commands/celeryd.py
@@ -1,20 +1,66 @@
+"""																																									
+																																									   
+ Start the celery daemon from the Django management command.																											
+																																									   
 """
+from django.core.management.base import BaseCommand,make_option
+from django.utils.daemonize import become_daemon
+from celery.bin.celeryd import run_worker, OPTION_LIST
+from os import path,getpid
+from string import atoi
+from django.conf import settings
+	
+class Command(BaseCommand):
+	"""Run the celery daemon."""
+	# Note: the filter of --version avoids a name conflict with																										
+	# manage.py's --version option.  Still feels a little yuck.																										
+	option_list = BaseCommand.option_list + \
+				   (make_option('--daemonize', action='store_true', dest='daemonize',
+								help='Runs celeryd as a daemon'),
+					make_option('--pidfile', help="Location of the file containing the process id"),
+					make_option('--stop', action='store_true', dest='stop',
+								help='Stop the currently running celeryd process')) +\
+				  filter(lambda opt: '--version' not in opt._long_opts, \
+						 OPTION_LIST)
+	help = 'Run celeryd as a daemon using django settings'
 
-Start the celery daemon from the Django management command.
+	def handle(self, *args, **options):
+		"""Handle the management command."""
+		daemonize = options.get('daemonize')
+		pidfile = options.get('pidfile')
+		stop = options.get('stop')
+		
+		# stop existing celeryd
+		stopped = stop_celery_process(pidfile)
+		if stop:
+			if not stopped:
+				print "No existing celeryd process"
+			quit()
+		
+		print "Starting celeryd (%s)" % getpid()
+		
+		# safely turn this process into a daemon
+		if daemonize:
+			become_daemon()
+		
+		# write the pidfile
+		if pidfile:
+			with open(pidfile,'w+') as f:
+				print "writing pidfile:",pidfile
+				f.write(str(getpid()))
+			
+		run_worker(**options)
 
-"""
-from celery.bin import celeryd
-
-from djcelery.management.base import CeleryCommand
-
-worker = celeryd.WorkerCommand()
-
-
-class Command(CeleryCommand):
-    """Run the celery daemon."""
-    help = 'Runs a Celery worker node.'
-    requires_model_validation = True
-    option_list = CeleryCommand.option_list + worker.get_options()
-
-    def handle(self, *args, **options):
-        worker.run(*args, **options)
+from os import kill,remove
+def stop_celery_process(pidfile):
+	if pidfile and path.exists(pidfile):
+		with open(pidfile,'r') as f:
+			pid = f.read()
+			try:
+				kill(atoi(pid),0)
+				print "Stopped celeryd process (%s)" % pid
+			except:
+				return False
+			remove(pidfile)
+		return True
+	
